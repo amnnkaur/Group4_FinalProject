6f7ac641c51404274faf45e9cc020ede9c0dd39d
macstudent <amankaur2628@gmail.com>
2020-01-30 20:57:00 -0500

    [Aman]Class Person: variables added

--- src/main/java/com/lambton/Person.java
+++ src/main/java/com/lambton/Person.java
@@ -1,6 +1,41 @@
 package com.lambton;
 
-public class Person
-{
-    
+
+import java.util.Date;
+
+enum prefix {
+    CUS,
+    DRI,
+    OWN
+}
+
+enum Gender {
+    Male,
+    Female,
+    Other
+}
+
+abstract class Person {
+    int personId;
+    String firstName;
+    String lastName;
+    Date birthDate;
+    Gender gender;
+    String email;
+    String address;
+    String contactNumber;
+    static String[] months = new String[]{"null",
+            "January",
+            "February",
+            "March",
+            "April",
+            "May",
+            "June",
+            "July",
+            "August",
+            "September",
+            "October",
+            "November",
+            "December"};
+
 }
