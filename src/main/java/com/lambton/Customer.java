6d9c9be0f28259810b7bbf2ea01cc770c11c0a28
macstudent <amankaur2628@gmail.com>
2020-01-30 21:14:20 -0500

    [Aman]Customer class: prefixPlacing() added

--- src/main/java/com/lambton/Customer.java
+++ src/main/java/com/lambton/Customer.java
@@ -2,6 +2,11 @@ package com.lambton;
 
 public class Customer extends Person
 {
+    public String prefixPlacing() {
+        String str = new String(String.valueOf(prefix.CUS) + "_");
+        str = str.concat(String.valueOf(personId));
+        return str;
+    }
 
     @Override
     public void finalExecution() {
