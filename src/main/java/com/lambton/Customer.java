6cf0b1f6681cb37cb31dcbc70a6045fc77cfb6d9
macstudent <amankaur2628@gmail.com>
2020-01-30 21:17:02 -0500

    [Aman]Customer class: setData added

--- src/main/java/com/lambton/Customer.java
+++ src/main/java/com/lambton/Customer.java
@@ -1,11 +1,25 @@
 package com.lambton;
 
+import java.util.Date;
+
 public class Customer extends Person
 {
     public String prefixPlacing() {
         String str = new String(String.valueOf(prefix.CUS) + "_");
         str = str.concat(String.valueOf(personId));
         return str;
+    }
+
+    @Override
+    public void setData(int personId,
+                        String firstName,
+                        String lastName,
+                        Date birthDate,
+                        Gender gender,
+                        String email,
+                        String address,
+                        String contactNumber) {
+        super.setData(personId, firstName, lastName, birthDate, gender, email, address, contactNumber);
     }
 
     @Override
