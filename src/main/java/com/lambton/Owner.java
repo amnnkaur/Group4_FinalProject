7e66a0c5cd39b693b5fe0dab065ba46987260259
macstudent <inshantt@gmail.com>
2020-01-31 20:35:13 -0500

    [inshant tageja] owner class - variable and constructor

--- src/main/java/com/lambton/Owner.java
+++ src/main/java/com/lambton/Owner.java
@@ -1,4 +1,14 @@
 package com.lambton;
 
-public class Owner {
+import java.util.Date;
+
+public class Owner extends Person {
+    String companyTitle;
+    String officeNumber;
+    String website;
+    String[] vehicleListOwned;
+
+    public Owner(int id, String firstName, String lastName, Gender gender, Date birthDate, String mobileNumber, String emailId, String userName, String password) {
+        super(id, firstName, lastName, gender, birthDate, mobileNumber, emailId, userName, password);
+    }
 }
