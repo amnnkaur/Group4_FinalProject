024241cbc1cbf611c9906d2a50377f54bf8022b0
Anmol <anmolsingh1597@gmail.com>
2020-02-01 15:52:38 -0500

    [Aman]Class Objects: Driver class tested

--- src/main/java/com/lambton/Objects.java
+++ src/main/java/com/lambton/Objects.java
@@ -9,5 +9,15 @@ public class Objects {
                 "9988421530", "me@me.com",
                 "Me", "ghtutgd", "Brampton", "Toronto");
         c1.printData();
+
+        Driver d1 = new Driver(10, "Anmol", "Singh", Gender.Male,
+                new Date(1997, 11, 15),
+                "9988421530", "me@me.com",
+                "Me", "anmol", 15479854, true, 5245.54d);
+        d1.printData();
+
+        d1.setDrivingHistoryCleared(false);
+        d1.printData();
+
     }
 }
