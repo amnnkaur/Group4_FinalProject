221d3bc7c5a6c95fb05aaf79c496fd27f3b5f109
macstudent <inshantt@gmail.com>
2020-01-31 20:31:58 -0500

    [inshant tageja] person and customer updated

--- src/main/java/com/lambton/Customer.java
+++ src/main/java/com/lambton/Customer.java
@@ -2,28 +2,51 @@ package com.lambton;
 
 import java.util.Date;
 
-public class Customer extends Person
-{
+public class Customer extends Person {
+    String address;
+    String city;
+    String[] vehicleRent;
+
+    public Customer(int id, String firstName, String lastName, Gender gender, Date birthDate,
+                    String mobileNumber, String emailId,
+                    String userName, String password,
+                    String address, String city) {
+        super(id, firstName, lastName, gender, birthDate, mobileNumber, emailId, userName, password);
+        this.address = address;
+        this.city = city;
+    }
+
     public String prefixPlacing() {
-        String str = new String(String.valueOf(prefix.CUS) + "_");
-        str = str.concat(String.valueOf(personId));
+        str = new String(String.valueOf(prefix.CUS) + "_");
+        str = str.concat(String.valueOf(id));
         return str;
     }
 
-    @Override
-    public void setData(int personId,
-                        String firstName,
-                        String lastName,
-                        Date birthDate,
-                        Gender gender,
-                        String email,
-                        String address,
-                        String contactNumber) {
-        super.setData(personId, firstName, lastName, birthDate, gender, email, address, contactNumber);
+    public String getAddress() {
+        return address;
     }
 
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+
     @Override
-    public void finalExecution() {
+    public void printData() {
+        System.out.println("Id: " + prefixPlacing());
+        super.printData();
+        System.out.println("Address: " + getAddress());
+        System.out.println("City: " + getCity());
+        System.out.println();
 
     }
+
 }
