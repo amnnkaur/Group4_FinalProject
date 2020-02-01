62672074ce1d2753baf3d0eae97ae8cd73233fcc
macstudent <inshantt@gmail.com>
2020-01-31 21:14:15 -0500

    [inshant tageja] class motorcycle-constructor updated

--- src/main/java/com/lambton/Motorcycle.java
+++ src/main/java/com/lambton/Motorcycle.java
@@ -1,10 +1,19 @@
 package com.lambton;
 
-public class Motorcycle extends Vehicle{
+public class Motorcycle extends Vehicle {
+
     double topSpeed;
     double milage;
 
-    public Motorcycle(long vehicleIdentificationNumber, String vehicleDescription, String manufacturerName, boolean isSelfDrive, boolean isInsured, int noOfSeat, Fuel fuelType) {
-        super(vehicleIdentificationNumber, vehicleDescription, manufacturerName, isSelfDrive, isInsured, noOfSeat, fuelType);
+    public Motorcycle(long vehicleIdentificationNumber, String vehicleDescription,
+                      String manufacturerName, boolean isSelfDrive,
+                      boolean isInsured, int noOfSeat, Fuel fuelType,
+                      double topSpeed, double milage) {
+        super(vehicleIdentificationNumber, vehicleDescription, manufacturerName,
+                isSelfDrive, isInsured, noOfSeat, fuelType);
+        super.vehicleType=Type.Motorcycle;
+        this.topSpeed = topSpeed;
+        this.milage = milage;
+
     }
 }
