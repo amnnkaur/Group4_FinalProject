16e16400e6d9eae6caee44fd3941692b4e31c7a9
macstudent <inshantt@gmail.com>
2020-01-31 20:36:59 -0500

    [inshant tageja] owner class - constructor update and vehicle list method added

--- src/main/java/com/lambton/Owner.java
+++ src/main/java/com/lambton/Owner.java
@@ -8,7 +8,19 @@ public class Owner extends Person {
     String website;
     String[] vehicleListOwned;
 
-    public Owner(int id, String firstName, String lastName, Gender gender, Date birthDate, String mobileNumber, String emailId, String userName, String password) {
+    public Owner(int id, String firstName, String lastName, Gender gender, Date birthDate,
+                 String mobileNumber, String emailId, String userName, String password,
+                 String companyTitle, String officeNumber, String website) {
         super(id, firstName, lastName, gender, birthDate, mobileNumber, emailId, userName, password);
+        this.companyTitle = companyTitle;
+        this.officeNumber = officeNumber;
+        this.website = website;
+    }
+    public String[] getVehicleListOwned() {
+        int i = 0;
+        for (i = 0; i < vehicleListOwned.length; i++) {
+            System.out.println("Vehicle " + (i + 1) + ": " + vehicleListOwned[i]);
+        }
+        return vehicleListOwned;
     }
 }
