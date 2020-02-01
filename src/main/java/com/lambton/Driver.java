407faf48e008223c30c62b6037429a3ecf0a376a
macstudent <inshantt@gmail.com>
2020-01-31 20:42:15 -0500

    [inshant tageja] Driver class constructor added

--- src/main/java/com/lambton/Driver.java
+++ src/main/java/com/lambton/Driver.java
@@ -1,4 +1,13 @@
 package com.lambton;
 
-public class Driver {
+import java.util.Date;
+
+public class Driver extends Person {
+    long licenceNumber;
+    boolean isDrivingHistoryCleared;
+    Double salary;
+
+    public Driver(int id, String firstName, String lastName, Gender gender, Date birthDate, String mobileNumber, String emailId, String userName, String password) {
+        super(id, firstName, lastName, gender, birthDate, mobileNumber, emailId, userName, password);
+    }
 }
