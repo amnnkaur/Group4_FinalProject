e7795f2f87e748f2bd32afc900ac3517c210f698
macstudent <inshantt@gmail.com>
2020-01-31 21:19:49 -0500

    [inshant tageja] constructor

--- src/main/java/com/lambton/Bus.java
+++ src/main/java/com/lambton/Bus.java
@@ -5,8 +5,26 @@ enum BusType{
     DoubleDecker,
     MiniCoach
 }
-public class Bus {
+public class Bus extends Vehicle {
+
+
     BusType typeOfBus;
     boolean isAccessibilityServiceAvailable;
     boolean isWifiAvailable;
+
+    public Bus(long vehicleIdentificationNumber, String vehicleDescription,
+               String manufacturerName, boolean isSelfDrive,
+            /*String driver,*/ boolean isInsured,
+            /* String insuranceNameProvider,*/ int noOfSeat, Fuel fuelType,
+               BusType typeOfBus, boolean isAccessibilityServiceAvailable,
+               boolean isWifiAvailable) {
+        super(vehicleIdentificationNumber, vehicleDescription,
+                manufacturerName, isSelfDrive,
+                /*driver,*/ isInsured,/* insuranceNameProvider,*/
+                noOfSeat, fuelType);
+        super.vehicleType=Type.Bus;
+        this.typeOfBus=typeOfBus;
+        this.isAccessibilityServiceAvailable=isAccessibilityServiceAvailable;
+        this.isWifiAvailable=isWifiAvailable;
+    }
 }
