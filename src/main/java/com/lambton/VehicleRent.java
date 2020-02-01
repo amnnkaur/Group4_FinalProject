895f751aefd58f6753eba01fcf8c654a774b2bfa
Anmol <anmolsingh1597@gmail.com>
2020-02-01 16:17:46 -0500

    [Anmol Singh]Class VehicleRent: Variables Added and Hashmap Tested

--- src/main/java/com/lambton/VehicleRent.java
+++ src/main/java/com/lambton/VehicleRent.java
@@ -1,4 +1,20 @@
 package com.lambton;
 
+import java.time.LocalDate;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
 public class VehicleRent {
+    LocalDate rentStartDate;
+    LocalDate rentEndDate;
+    int rentedDays;
+    static HashMap<String, String> vehicle = new HashMap<>();
+
+    public static void main(String[] args) {
+ vehicle.put("InD","INDIA");
+        for(Map.Entry<String,String>entry:vehicle.entrySet()) {
+            System.out.println(entry.getKey() + " - " + entry.getValue());
+        }
+    }
 }
