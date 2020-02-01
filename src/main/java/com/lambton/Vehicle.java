75579a57ebca5c2d5eb8f7d39605b2b12edf0e4c
macstudent <inshantt@gmail.com>
2020-01-31 20:53:04 -0500

    [inshant tageja] vehicle abstract - enum and variables declared

--- src/main/java/com/lambton/Vehicle.java
+++ src/main/java/com/lambton/Vehicle.java
@@ -1,6 +1,29 @@
 package com.lambton;
 
-public abstract class Vehicle
-{
 
+enum Fuel {
+    Petrol,
+    Diesel,
+    Electric
+}
+
+enum Type {
+    Car,
+    Motorcycle,
+    Bus
+}
+public abstract class Vehicle {
+    String str;
+    long vehicleIdentificationNumber;
+    String vehicleDescription;
+    String manufacturerName;
+    boolean isSelfDrive;
+    String Driver;
+    boolean isInsured;
+    String insuranceNameProvider;
+    int noOfSeat;
+    Fuel fuelType;
+    Type vehicleType;
+    int baseRate;
+    int ratePerKm;
 }
