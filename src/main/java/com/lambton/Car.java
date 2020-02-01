c613f151eef1988efe368755ba5bc558ad2d1171
macstudent <inshantt@gmail.com>
2020-01-31 21:03:56 -0500

    [inshant tageja] class car-constructed generated

--- src/main/java/com/lambton/Car.java
+++ src/main/java/com/lambton/Car.java
@@ -1,4 +1,22 @@
-package com.lambton;
 
-public class Car {
+import com.lambton;
+
+enum CarType {
+    Hatchback,
+    Sedan,
+    MPV,
+    SUV,
+    Crossover,
+    Coupe,
+    Convertible
+}
+
+public class Car extends Vehicle {
+
+    CarType carType;
+    String carColor;
+
+    public Car(long vehicleIdentificationNumber, String vehicleDescription, String manufacturerName, boolean isSelfDrive, boolean isInsured, int noOfSeat, Fuel fuelType) {
+        super(vehicleIdentificationNumber, vehicleDescription, manufacturerName, isSelfDrive, isInsured, noOfSeat, fuelType);
+    }
 }
