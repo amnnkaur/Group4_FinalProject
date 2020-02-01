dec67d785535b80e010eba8811b5e7b8541f901a
macstudent <inshantt@gmail.com>
2020-01-31 20:55:33 -0500

    [inshant tageja] vehicle abstract - enum updated, constructor added and functionality added

--- src/main/java/com/lambton/Vehicle.java
+++ src/main/java/com/lambton/Vehicle.java
@@ -1,6 +1,6 @@
 package com.lambton;
 
-
+import java.util.Scanner;
 enum Fuel {
     Petrol,
     Diesel,
@@ -26,4 +26,37 @@ public abstract class Vehicle {
     Type vehicleType;
     int baseRate;
     int ratePerKm;
+
+    public Vehicle(long vehicleIdentificationNumber, String vehicleDescription, String manufacturerName, boolean isSelfDrive,
+            /*String driver,*/ boolean isInsured, /*String insuranceNameProvider,*/
+                   int noOfSeat, Fuel fuelType/*, Type vehicleType, int baseRate, int ratePerKm*/) {
+        this.vehicleIdentificationNumber = vehicleIdentificationNumber;
+        this.vehicleDescription = vehicleDescription;
+        this.manufacturerName = manufacturerName;
+        this.isSelfDrive = isSelfDrive;
+        if(isSelfDrive==false){
+            System.out.println("Enter Driver's Name: ");
+            Scanner inputDriver=new Scanner(System.in);
+            String driver=inputDriver.nextLine();
+            this.Driver = driver;
+        }
+        else{
+            this.Driver=null;
+        }
+        this.isInsured = isInsured;
+        if(isInsured==true){
+            System.out.println("Input Insurance Name Provider: ");
+            Scanner inputInsuranceProvider= new Scanner(System.in);
+            String insuranceNameProvider=inputInsuranceProvider.nextLine();
+            this.insuranceNameProvider = insuranceNameProvider;
+        }else{
+            this.insuranceNameProvider=null;
+        }
+
+        this.noOfSeat = noOfSeat;
+        this.fuelType = fuelType;
+//        this.vehicleType = vehicleType;
+       /* this.baseRate = baseRate;
+        this.ratePerKm = ratePerKm;*/
+    }
 }
