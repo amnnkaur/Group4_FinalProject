fa4ffd4bcfa3c1e7528745103e7a87e49bea092c
macstudent <inshantt@gmail.com>
2020-01-31 20:42:52 -0500

    [inshant tageja] Driver class constructor updated

--- src/main/java/com/lambton/Driver.java
+++ src/main/java/com/lambton/Driver.java
@@ -7,7 +7,12 @@ public class Driver extends Person {
     boolean isDrivingHistoryCleared;
     Double salary;
 
-    public Driver(int id, String firstName, String lastName, Gender gender, Date birthDate, String mobileNumber, String emailId, String userName, String password) {
+    public Driver(int id, String firstName, String lastName, Gender gender, Date birthDate, String mobileNumber,
+                  String emailId, String userName, String password,
+                  long licenceNumber, boolean isDrivingHistoryCleared, double salary) {
         super(id, firstName, lastName, gender, birthDate, mobileNumber, emailId, userName, password);
+        this.licenceNumber = licenceNumber;
+        this.isDrivingHistoryCleared = isDrivingHistoryCleared;
+        this.salary = salary;
     }
 }
