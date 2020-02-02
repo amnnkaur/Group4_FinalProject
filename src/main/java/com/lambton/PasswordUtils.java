9fe74a005a71fbc010ca3ce0c6b63962fab9b2dc
Amanpreet Kaur <amankaur2628@gmail.com>
2020-02-03 03:53:01 +0530

    [Aman]Class PasswordUtils: Hashing the password code block

--- src/main/java/com/lambton/PasswordUtils.java
+++ src/main/java/com/lambton/PasswordUtils.java
@@ -1,6 +1,11 @@
 package com.lambton;
 
+import javax.crypto.SecretKeyFactory;
+import javax.crypto.spec.PBEKeySpec;
+import java.security.NoSuchAlgorithmException;
 import java.security.SecureRandom;
+import java.security.spec.InvalidKeySpecException;
+import java.util.Arrays;
 import java.util.Random;
 
 public class PasswordUtils {
@@ -17,4 +22,18 @@ public class PasswordUtils {
         }
         return new String(returnValue);
     }
+
+    public static byte[] hash(char[] password, byte[] salt) {
+        PBEKeySpec spec = new PBEKeySpec(password, salt, Iterations, Count);
+        Arrays.fill(password, Character.MIN_VALUE);
+        try {
+            SecretKeyFactory skf = SecretKeyFactory.getInstance("PBKDF2WithHmacSHA1");
+            return skf.generateSecret(spec).getEncoded();
+        } catch (NoSuchAlgorithmException | InvalidKeySpecException e) {
+            throw new AssertionError("Error while hashing a password: " + e.getMessage(), e);
+        } finally {
+            spec.clearPassword();
+        }
+    }
+
 }
