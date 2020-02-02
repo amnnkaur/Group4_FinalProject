80a142f3c9161ca5e7fd22337b616679b737b19f
Amanpreet Kaur <amankaur2628@gmail.com>
2020-02-03 04:05:55 +0530

    [Aman]Class PasswordUtils: Updated and tested

--- src/main/java/com/lambton/PasswordUtils.java
+++ src/main/java/com/lambton/PasswordUtils.java
@@ -1,16 +1,14 @@
 package com.lambton;
-
-import javax.crypto.SecretKeyFactory;
-import javax.crypto.spec.PBEKeySpec;
 import java.security.NoSuchAlgorithmException;
 import java.security.SecureRandom;
 import java.security.spec.InvalidKeySpecException;
 import java.util.Arrays;
 import java.util.Base64;
 import java.util.Random;
+import javax.crypto.SecretKeyFactory;
+import javax.crypto.spec.PBEKeySpec;
 
 public class PasswordUtils {
-
     private static final Random RANDOM = new SecureRandom();
     private static final String ALPHABET = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
     private static final int Iterations = 10000;
@@ -23,9 +21,8 @@ public class PasswordUtils {
         }
         return new String(returnValue);
     }
-
     public static byte[] hash(char[] password, byte[] salt) {
-        PBEKeySpec spec = new PBEKeySpec(password, salt, Iterations, Count);
+        PBEKeySpec spec = new PBEKeySpec(password, salt, Iterations,Count);
         Arrays.fill(password, Character.MIN_VALUE);
         try {
             SecretKeyFactory skf = SecretKeyFactory.getInstance("PBKDF2WithHmacSHA1");
@@ -54,4 +51,5 @@ public class PasswordUtils {
 
         return returnValue;
     }
+
 }
