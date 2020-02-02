bc10fb4086017d18121285e6f9532a831ace75f4
Amanpreet Kaur <amankaur2628@gmail.com>
2020-02-03 03:48:10 +0530

    [Aman]Class PasswordUtils: Variables and `Random` import

--- src/main/java/com/lambton/PasswordUtils.java
+++ src/main/java/com/lambton/PasswordUtils.java
@@ -1,4 +1,12 @@
 package com.lambton;
 
+import java.security.SecureRandom;
+import java.util.Random;
+
 public class PasswordUtils {
+
+    private static final Random RANDOM = new SecureRandom();
+    private static final String ALPHABET = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+    private static final int Iterations = 10000;
+    private static final int Count = 256;
 }
