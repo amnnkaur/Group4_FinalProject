665dfcc5504ede6502a2fc1aba785ab71c0fd2ab
Amanpreet Kaur <amankaur2628@gmail.com>
2020-02-03 03:49:01 +0530

    [Aman]Class PasswordUtils: getSalt() added

--- src/main/java/com/lambton/PasswordUtils.java
+++ src/main/java/com/lambton/PasswordUtils.java
@@ -9,4 +9,12 @@ public class PasswordUtils {
     private static final String ALPHABET = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
     private static final int Iterations = 10000;
     private static final int Count = 256;
+
+    public static String getSalt(int length) {
+        StringBuilder returnValue = new StringBuilder(length);
+        for (int i = 0; i < length; i++) {
+            returnValue.append(ALPHABET.charAt(RANDOM.nextInt(ALPHABET.length())));
+        }
+        return new String(returnValue);
+    }
 }
